27225e04d3e9ee649123ac6dbdc7082095946bab
Michele Di Maio <michele.dimaio@gmail.com>
2020-05-07 00:20:14 +0200

    - Join sigla e spezzone (fuck yeah)

--- digisplit.py
+++ digisplit.py
@@ -9,6 +9,8 @@ def find_between(s, start, end):
 
 input_video = "Digitalia.fm #521 - Monopattini Tattici Nucleari.mp4" 
 base_dir = "/home/micheledm/git/digisplit/"
+vd_ext = "mp4"
+
 input_video = base_dir + input_video #.replace(" ","\ ")
 cue_file = 'chapters.cue'
 
@@ -54,13 +56,31 @@ for line in d:
             cue_end_p = current_cue
             FMT = '%H:%M:%S'
             tracks[i-1]['duration']  = str(datetime.strptime(cue_end_p, FMT) - datetime.strptime(cue_start_p, FMT))
+            fade = tracks[i-1]['duration'].split(":")
+            fade = int(fade[0])*3600 + int(fade[1])*60 + int(fade[2]) - 1
+            tracks[i-1]['fade'] = fade
+
+print(tracks)
 
 for i in tracks:
     if "cue_start" in tracks[i] and "cue_end" in tracks[i]:
-        i = 3
-        
+        i = 2
+
         print("Cutting segment %s: %s (%s/%s-%s)" % (tracks[i]["number"], tracks[i]["title"],tracks[i]["cue_start"],tracks[i]["cue_end"],tracks[i]["duration"]))
-        output_video = base_dir + str(i) + "-" + tracks[i]["title"] + '.mp4'
-        
-        subprocess.call(['ffmpeg', '-i', input_video, '-ss', tracks[i]["cue_start"], '-t', tracks[i]["duration"], '-async', '1', '-strict', '2', output_video])
+        output_video = base_dir + str(i) + "-" + tracks[i]["title"] + '.' + vd_ext
+
+        #TAGLIA VIDEO IN BASE AI CUE
+        subprocess.call(['ffmpeg', '-i', input_video, '-y', '-ss', tracks[i]["cue_start"], '-t', tracks[i]["duration"], '-async', '1', '-strict', '2', 'tmp_a.' + vd_ext])
+
+
+        #INSERISCE FADE INGRESSO E USCITA
+        subprocess.call(['ffmpeg', '-i', 'tmp_a.' + vd_ext, '-y', '-vf', 'fade=type=in:duration=1,fade=type=out:duration=1:start_time='+ str(tracks[i]["fade"]), 'tmp_b.'  + vd_ext ])
+
+        #PREPARA TS PER TRANSCODIFICAs     
+        subprocess.call(['ffmpeg', '-i', 'tmp_b.' + vd_ext, '-c', 'copy', '-bsf:v', 'h264_mp4toannexb', '-y', '-f', 'mpegts', 'tmp_c.ts'])
+            
+        #JOIN SIGLA E VIDEO
+        subprocess.call(['ffmpeg', '-f', 'mpegts', '-i', 'concat:intro.ts|tmp_c.ts', '-y', '-bsf:a', 'aac_adtstoasc', output_video])
+
         break
+
